bf4b97250e4c2d2f7e43231dc5f4e5824fcd05f1
Joomy <vlastimil.vasek@frendo.cz>
2018-11-05 15:22:46 +0100

    předprodukční verze, ng7, datově přes www.hypozor.cz

---
+++ hypoteky/assets/params/loga.ts
@@ -0,0 +1,13 @@
+export const LOGO_200x100 = {
+    'RB': 'https://www.srovnavac.cz/images/loga/200x100/rb.png',
+    'ČSOB': 'https://www.srovnavac.cz/images/loga/200x100/csob-banka.png',
+    'HB': 'https://www.srovnavac.cz/images/loga/200x100/hb.png',
+    'WHB': 'https://www.srovnavac.cz/images/loga/200x100/wustenrot.png',
+    'Equa bank': 'https://www.srovnavac.cz/images/loga/200x100/equabank.png',
+    'Mbank': 'https://www.srovnavac.cz/images/loga/200x100/mbank.png',
+    'KB': 'https://www.srovnavac.cz/images/loga/200x100/kb.png',
+    'MMB': 'https://www.srovnavac.cz/images/loga/200x100/moneta.png',
+    'UniCredit': 'https://www.srovnavac.cz/images/loga/200x100/unicredit.png',
+    'Sberbank': 'https://www.srovnavac.cz/images/loga/200x100/sberbank.png',
+    'ČS': 'https://www.srovnavac.cz/images/loga/200x100/cs.png'
+};
