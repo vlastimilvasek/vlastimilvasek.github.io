a69531d26b759ca260772d5529d1a203af28bf7f
Joomy <vlastimil.vasek@frendo.cz>
2019-01-16 10:06:32 +0100

    druhá verze odpovědnosti

---
+++ odpovednost-puvodni/assets/params/loga.ts
@@ -0,0 +1,9 @@
+export const LOGO_200x100 = {
+    'Axa': 'https://www.srovnavac.cz/images/loga/200x100/axa.png',
+    'ČSOB Pojišťovna': 'https://www.srovnavac.cz/images/loga/200x100/csob.png',
+    'Direct': 'https://www.srovnavac.cz/images/loga/200x100/direct.png',
+    'Ergo': 'https://www.srovnavac.cz/images/loga/200x100/ergo.png',
+    'Slavia': 'https://www.srovnavac.cz/images/loga/200x100/slavia.png',
+    'a': '',
+    'Generali': 'https://www.srovnavac.cz/images/loga/200x100/generali.png'
+};
